ec1ef8129eeb7086160b63250c18d9c212fad22b
chanhee han <mh981219@naver.com>
2024-09-11 17:36:39 +0900

    4 DB연결 config클래스, 5 메타데이터 테이블, 6 배치처리1 테이블 to 테이블, 7 배치처리1 실행 및 스케쥴

---
+++ springbatch/src/main/java/com/chanhee/repository/AfterRepository.java
@@ -0,0 +1,9 @@
+package com.chanhee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.chanhee.entity.AfterEntity;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+
+}
---
+++ springbatch/src/main/java/com/chanhee/repository/BeforeRepository.java
@@ -0,0 +1,9 @@
+package com.chanhee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.chanhee.entity.BeforeEntity;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long>{
+
+}
